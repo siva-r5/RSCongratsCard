42b194071133b6358e9855b148000962a1e89598
siva <sivaganesh8278@gmail.com>
2023-01-31 08:14:50 +0000

    congratsCard

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="bg-container">
+    <div>
+      <h1 className="heading">Congratulations</h1>
+    </div>
+    <div className="sub-container">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <div>
+        <h1 className="sub-heading">Chandramma </h1>
+        <p>vishnu Institute of Computer Education and Technology,Bhimavaram</p>
+      </div>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
